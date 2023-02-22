f7a513351f5c959cfb1850fa8c3da7fcfd706fbd
MarialuizaLeao <maria_luiza1598@hotmail.com>
2023-02-21 23:56:54 -0300

    token safity

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // variable to hold reference to the discord.js node module
 const Discord = require("discord.js");  // para acessar a aplicação
 
+// contains a string that is the password/token for the discord bot
+const { token } = require("./config.json")
+
 // Gateway Intents were introduced by Discord so bot developers can choose
 // which events their bot receives based on which data it needs to function
 // With partials we will be able to receive the full data of the objects returned from each event
@@ -27,4 +30,4 @@ Client.on("ready", (client) => {
 })
 
 // Logs in the discord bot with the password stored in an external file
-Client.login("MTA3NjkzNTk2MDk3NTExNDM4MA.GhKl37.5sGe30Q2m9qlu0Cr4K3EXLUulrp3ILJDrzvx2I");
+Client.login(token);
